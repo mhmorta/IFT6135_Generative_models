2d59355069f27c3ddce43fcb7fc00761d729da4c
ggegiya1 <gegiya@gmail.com>
2019-04-14 18:31:05 -0400

    fix encoder and decoder, now it's able to train

--- vae/model.py
+++ vae/model.py
@@ -1,16 +1,19 @@
 from __future__ import print_function
 import torch
 import torch.utils.data
-from torch import nn, optim
-from torch.nn import functional as F
+from torch import nn
 
 
 class Flatten(nn.Module):
     def forward(self, input):
         return input.view(input.size(0), -1)
 
+class UnFlatten(nn.Module):
+    def forward(self, input, size=256):
+        return input.view(input.size(0), size, 1, 1)
+
 class VAE(nn.Module):
-    def __init__(self):
+    def __init__(self, hidden_features=100):
         super(VAE, self).__init__()
 
         self.encoder = nn.Sequential(
@@ -22,13 +25,16 @@ class VAE(nn.Module):
             nn.AvgPool2d(kernel_size = 2, stride = 2),
             nn.Conv2d(64, 256, kernel_size = (5, 5)),
             nn.ELU(),
-            Flatten(),
-            nn.Linear(in_features = 256, out_features = 100)
+            Flatten()
         )
 
+        self.encoder_mean = nn.Linear(in_features = 256, out_features = hidden_features)
+        self.encoder_logvar = nn.Linear(in_features = 256, out_features = hidden_features)
+
         self.decoder = nn.Sequential(
-            nn.Linear(in_features = 100, out_features = 256),
+            nn.Linear(in_features = hidden_features, out_features = 256),
             nn.ELU(),
+            UnFlatten(),
             nn.Conv2d(256, 64, kernel_size = (5, 5), padding = (4, 4)),
             nn.ELU(),
             nn.UpsamplingBilinear2d(scale_factor = 2),
@@ -41,32 +47,25 @@ class VAE(nn.Module):
         )
 
     def encode(self, x):
-        return self.encoder(x)
+        h = self.encoder(x)
+        return self.encoder_mean(h), self.encoder_logvar(h)
 
     def reparameterize(self, mu, logvar):
-        std = torch.exp(0.5*logvar)
+
+        # convert to the standard deviation
+        std = torch.exp(0.5 * logvar)
+
+        # random sampling from normal distribution (0, 1)
         eps = torch.randn_like(std)
-        return mu + eps*std
+
+        # reparametrize
+        return mu + eps * std
 
     def decode(self, z):
         return torch.sigmoid(self.decoder(z))
 
     def forward(self, x):
-        mu, logvar = self.encode(x)
-        z = self.reparameterize(mu, logvar)
-        return self.decode(z), mu, logvar
+        mean, logvar = self.encode(x)
+        z = self.reparameterize(mean, logvar)
+        return self.decode(z), mean, logvar
 
-# Reconstruction + KL divergence losses summed over all elements and batch
-def loss_function(recon_x, x, mu, logvar):
-    BCE = F.binary_cross_entropy(recon_x, x.view(-1, 784), reduction='sum')
-
-    # see Appendix B from VAE paper:
-    # Kingma and Welling. Auto-Encoding Variational Bayes. ICLR, 2014
-    # https://arxiv.org/abs/1312.6114
-    # 0.5 * sum(1 + log(sigma^2) - mu^2 - sigma^2)
-
-    # TODO confirm if we do need this?
-    # KLD = -0.5 * torch.sum(1 + logvar - mu.pow(2) - logvar.exp())
-    #return BCE + KLD
-
-    return BCE
--- vae/train.py
+++ vae/train.py
@@ -3,16 +3,19 @@ Code based on the vae example at: https://raw.githubusercontent.com/pytorch/exam
 """
 import argparse
 import torch.utils.data
-from torch import nn, optim
+from torch import optim
 from torchvision.utils import save_image
-from vae.model import VAE, loss_function
-from vae.dataloader import binarized_mnist_data_loader
+from vae.model import VAE
+from vae.dataloader import binarized_mnist_data_loader, MNIST_IMAGE_SIZE
+from torch.nn import functional as F
 
 
 parser = argparse.ArgumentParser(description='VAE MNIST Example')
 parser.add_argument('--batch-size', type=int, default=64, metavar='N',
                     help='input batch size for training (default: 128)')
-parser.add_argument('--epochs', type=int, default=10, metavar='N',
+parser.add_argument('--hidden-features', type=int, default=100, metavar='N',
+                    help='latent variable size')
+parser.add_argument('--epochs', type=int, default=1, metavar='N',
                     help='number of epochs to train (default: 10)')
 parser.add_argument('--no-cuda', action='store_true', default=False,
                     help='enables CUDA training')
@@ -27,10 +30,26 @@ torch.manual_seed(args.seed)
 
 device = torch.device("cuda" if args.cuda else "cpu")
 
-model = VAE().to(device)
+model = VAE(args.hidden_features).to(device)
+
 optimizer = optim.Adam(model.parameters(), lr=1e-3)
 
-train_loader, valid_loader, test_loader = binarized_mnist_data_loader("binarized_mnist", 64)
+train_loader, valid_loader, test_loader = binarized_mnist_data_loader("binarized_mnist", args.batch_size)
+
+# Reconstruction + KL divergence losses summed over all elements and batch
+def loss_function(recon_x, x, mu, logvar):
+    BCE = F.binary_cross_entropy(recon_x, x.view(-1, MNIST_IMAGE_SIZE * MNIST_IMAGE_SIZE), reduction='sum')
+
+    # see Appendix B from VAE paper:
+    # Kingma and Welling. Auto-Encoding Variational Bayes. ICLR, 2014
+    # https://arxiv.org/abs/1312.6114
+    # 0.5 * sum(1 + log(sigma^2) - mu^2 - sigma^2)
+
+    # TODO confirm if we do need this?
+    # KLD = -0.5 * torch.sum(1 + logvar - mu.pow(2) - logvar.exp())
+    #return BCE + KLD
+
+    return BCE
 
 def train(epoch):
     model.train()
@@ -64,7 +83,7 @@ def test(epoch):
             test_loss += loss_function(recon_batch, data, mu, logvar).item()
             if i == 0:
                 n = min(data.size(0), 8)
-                comparison = torch.cat([data[:n], recon_batch.view(args.batch_size, 1, 28, 28)[:n]])
+                comparison = torch.cat([data[:n], recon_batch.view(args.batch_size, 1, MNIST_IMAGE_SIZE, MNIST_IMAGE_SIZE)[:n]])
                 save_image(comparison.cpu(),
                          'results/reconstruction_' + str(epoch) + '.png', nrow=n)
 
@@ -76,7 +95,7 @@ for epoch in range(1, args.epochs + 1):
     train(epoch)
     test(epoch)
     with torch.no_grad():
-        sample = torch.randn(args.batch_size, 20).to(device)
+        sample = torch.randn(args.batch_size, args.hidden_features).to(device)
         sample = model.decode(sample).cpu()
-        save_image(sample.view(args.batch_size, 1, 28, 28),
+        save_image(sample.view(args.batch_size, 1, MNIST_IMAGE_SIZE, MNIST_IMAGE_SIZE),
                    'results/sample_' + str(epoch) + '.png')
