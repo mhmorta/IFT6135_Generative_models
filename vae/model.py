b408bda8e5170c734c11562ddd47d677e5545b62
layaars <laya.rafiee@gmail.com>
2019-04-17 18:53:08 -0400

    Some updates

--- vae/model.py
+++ vae/model.py
@@ -1,37 +1,57 @@
 from __future__ import print_function
 import torch
 import torch.utils.data
-from torch import nn, optim
-from torch.nn import functional as F
+from torch import nn
 
 
 class Flatten(nn.Module):
     def forward(self, input):
         return input.view(input.size(0), -1)
 
+
+class UnFlatten(nn.Module):
+    def forward(self, input, size=256):
+        return input.view(input.size(0), size, 1, 1)
+
+
+# source: https://discuss.pytorch.org/t/using-nn-function-interpolate-inside-nn-sequential/23588
+class Interpolate(nn.Module):
+    def __init__(self, scale_factor):
+        super(Interpolate, self).__init__()
+        self.interp = nn.functional.interpolate
+        self.scale_factor = scale_factor
+
+    def forward(self, x):
+        x = self.interp(x, scale_factor=self.scale_factor, mode='bilinear')
+        return x
+
+
 class VAE(nn.Module):
-    def __init__(self):
+    def __init__(self, hidden_features):
         super(VAE, self).__init__()
 
         self.encoder = nn.Sequential(
             nn.Conv2d(1, 32, kernel_size=(3, 3)),
             nn.ELU(),
             nn.AvgPool2d(kernel_size=2, stride=2),
-            nn.Conv2d(32, 64, kernel_size = (3, 3)),
+            nn.Conv2d(32, 64, kernel_size=(3, 3)),
             nn.ELU(),
-            nn.AvgPool2d(kernel_size = 2, stride = 2),
-            nn.Conv2d(64, 256, kernel_size = (5, 5)),
+            nn.AvgPool2d(kernel_size=2, stride=2),
+            nn.Conv2d(64, 256, kernel_size=(5, 5)),
             nn.ELU(),
-            Flatten(),
-            nn.Linear(in_features = 256, out_features = 100)
+            Flatten()
         )
 
+        self.encoder_mean = nn.Linear(in_features=256, out_features = hidden_features)
+        self.encoder_logvar = nn.Linear(in_features=256, out_features = hidden_features)
+
         self.decoder = nn.Sequential(
-            nn.Linear(in_features = 100, out_features = 256),
+            nn.Linear(in_features = hidden_features, out_features = 256),
             nn.ELU(),
+            UnFlatten(),
             nn.Conv2d(256, 64, kernel_size = (5, 5), padding = (4, 4)),
             nn.ELU(),
-            nn.UpsamplingBilinear2d(scale_factor = 2),
+            nn.UpsamplingBilinear2d(scale_factor=2),
             nn.Conv2d(64, 32, kernel_size = (3, 3), padding = (2, 2)),
             nn.ELU(),
             nn.UpsamplingBilinear2d(scale_factor = 2),
@@ -41,32 +61,25 @@ class VAE(nn.Module):
         )
 
     def encode(self, x):
-        return self.encoder(x)
+        h = self.encoder(x)
+        return self.encoder_mean(h), self.encoder_logvar(h)
 
     def reparameterize(self, mu, logvar):
-        std = torch.exp(0.5*logvar)
+
+        # convert to the standard deviation
+        std = torch.exp(0.5 * logvar)
+
+        # random sampling from normal distribution (0, 1)
         eps = torch.randn_like(std)
-        return mu + eps*std
+
+        # reparametrize
+        return mu + eps * std
 
     def decode(self, z):
         return torch.sigmoid(self.decoder(z))
 
     def forward(self, x):
-        mu, logvar = self.encode(x)
-        z = self.reparameterize(mu, logvar)
-        return self.decode(z), mu, logvar
+        mean, logvar = self.encode(x)
+        z = self.reparameterize(mean, logvar)
+        return self.decode(z), mean, logvar
 
-# Reconstruction + KL divergence losses summed over all elements and batch
-def loss_function(recon_x, x, mu, logvar):
-    BCE = F.binary_cross_entropy(recon_x, x.view(-1, 784), reduction='sum')
-
-    # see Appendix B from VAE paper:
-    # Kingma and Welling. Auto-Encoding Variational Bayes. ICLR, 2014
-    # https://arxiv.org/abs/1312.6114
-    # 0.5 * sum(1 + log(sigma^2) - mu^2 - sigma^2)
-
-    # TODO confirm if we do need this?
-    # KLD = -0.5 * torch.sum(1 + logvar - mu.pow(2) - logvar.exp())
-    #return BCE + KLD
-
-    return BCE
--- vae/train.py
+++ vae/train.py
@@ -3,16 +3,22 @@ Code based on the vae example at: https://raw.githubusercontent.com/pytorch/exam
 """
 import argparse
 import torch.utils.data
-from torch import nn, optim
+from torch import optim
 from torchvision.utils import save_image
-from vae.model import VAE, loss_function
-from vae.dataloader import binarized_mnist_data_loader
+from vae.model import VAE
+from vae.dataloader import binarized_mnist_data_loader, MNIST_IMAGE_SIZE
+from torch.nn import functional as F
+import os
 
 
 parser = argparse.ArgumentParser(description='VAE MNIST Example')
 parser.add_argument('--batch-size', type=int, default=64, metavar='N',
                     help='input batch size for training (default: 128)')
-parser.add_argument('--epochs', type=int, default=10, metavar='N',
+parser.add_argument('--max-batch-idx', type=int, default=99999, metavar='N',
+                    help='only for debugging locally')
+parser.add_argument('--hidden-features', type=int, default=100, metavar='N',
+                    help='latent variable size')
+parser.add_argument('--epochs', type=int, default=20, metavar='N',
                     help='number of epochs to train (default: 10)')
 parser.add_argument('--no-cuda', action='store_true', default=False,
                     help='enables CUDA training')
@@ -27,16 +33,45 @@ torch.manual_seed(args.seed)
 
 device = torch.device("cuda" if args.cuda else "cpu")
 
-model = VAE().to(device)
-optimizer = optim.Adam(model.parameters(), lr=1e-3)
+model = VAE(args.hidden_features).to(device)
 
-train_loader, valid_loader, test_loader = binarized_mnist_data_loader("binarized_mnist", 64)
+optimizer = optim.Adam(model.parameters(), lr=3e-4)
+
+current_dir = os.path.dirname(os.path.realpath(__file__))
+results_dir = '{}/results'.format(current_dir)
+saved_model = '{}/saved_model'.format(current_dir)
+
+
+# Reconstruction + KL divergence losses summed over all elements and batch
+def loss_function(recon_x, x, mu, logvar):
+    # E[log P(X|z)]
+    # fidelity loss
+    # https://youtu.be/Hnns75GNUzs?list=PLdxQ7SoCLQANizknbIiHzL_hYjEaI-wUe&t=608
+    # todo reduce_sum or reduce_mean? https://youtu.be/Hnns75GNUzs?list=PLdxQ7SoCLQANizknbIiHzL_hYjEaI-wUe&t=739
+    logx_z_likelihood = -F.binary_cross_entropy(recon_x.view(args.batch_size, -1), x.view(args.batch_size, -1), reduction='sum')
+
+    # Compute the divergence D_KL[q(z|x)||p(z)]
+    # given z ~ N(0, 1)
+    # see Appendix B from VAE paper:
+    # Kingma and Welling. Auto-Encoding Variational Bayes. ICLR, 2014
+    # https://arxiv.org/abs/1312.6114
+    # 0.5 * sum(1 + log(sigma^2) - mu^2 - sigma^2)
+    KLD = -0.5 * torch.sum(1 + logvar - mu.pow(2) - logvar.exp())
+    # todo if we change it to negative value adjust the saving best model logic in main
+
+    ELBO = logx_z_likelihood - KLD
+
+    # optimizer will minimize loss function, thus in order to maximize ELBO we have to negate it, i.e loss = -ELBO
+    return -ELBO
+
 
 def train(epoch):
     model.train()
     train_loss = 0
 
     for batch_idx, data in enumerate(train_loader):
+        if batch_idx > args.max_batch_idx:
+            break
         data = data.to(device)
         optimizer.zero_grad()
         recon_batch, mu, logvar = model(data)
@@ -54,7 +89,27 @@ def train(epoch):
           epoch, train_loss / len(train_loader.dataset)))
 
 
-def test(epoch):
+def validate(epoch):
+    model.eval()
+    test_loss = 0
+
+    with torch.no_grad():
+        for i, data in enumerate(valid_loader):
+            data = data.to(device)
+            recon_batch, mu, logvar = model(data)
+            test_loss += loss_function(recon_batch, data, mu, logvar).item()
+            if i == 0:
+                n = min(data.size(0), 8)
+                comparison = torch.cat([data[:n], recon_batch.view(args.batch_size, 1, MNIST_IMAGE_SIZE, MNIST_IMAGE_SIZE)[:n]])
+                save_image(comparison.cpu(),
+                         '{}/reconstruction_{}.png'.format(results_dir, epoch), nrow=n)
+
+    test_loss /= len(valid_loader.dataset)
+    print('====> Average Validation loss: {:.4f}'.format(test_loss))
+    return test_loss
+
+
+def test():
     model.eval()
     test_loss = 0
     with torch.no_grad():
@@ -62,21 +117,33 @@ def test(epoch):
             data = data.to(device)
             recon_batch, mu, logvar = model(data)
             test_loss += loss_function(recon_batch, data, mu, logvar).item()
-            if i == 0:
-                n = min(data.size(0), 8)
-                comparison = torch.cat([data[:n], recon_batch.view(args.batch_size, 1, 28, 28)[:n]])
-                save_image(comparison.cpu(),
-                         'results/reconstruction_' + str(epoch) + '.png', nrow=n)
 
     test_loss /= len(test_loader.dataset)
-    print('====> Test set loss: {:.4f}'.format(test_loss))
+    print('====> Average Test loss: {:.4f}'.format(test_loss))
 
 
-for epoch in range(1, args.epochs + 1):
-    train(epoch)
-    test(epoch)
+def sample(epoch):
+    model.eval()
     with torch.no_grad():
-        sample = torch.randn(args.batch_size, 20).to(device)
+        sample = torch.randn(args.batch_size, args.hidden_features).to(device)
         sample = model.decode(sample).cpu()
-        save_image(sample.view(args.batch_size, 1, 28, 28),
-                   'results/sample_' + str(epoch) + '.png')
+        save_image(sample.view(args.batch_size, 1, MNIST_IMAGE_SIZE, MNIST_IMAGE_SIZE),
+                   '{}/sample_{}.png'.format(results_dir, epoch))
+
+
+if __name__ == '__main__':
+    train_loader, valid_loader, test_loader = binarized_mnist_data_loader('{}/binarized_mnist'.format(current_dir),
+                                                                          args.batch_size)
+    best_valid_loss = None
+    for epoch in range(1, args.epochs + 1):
+        train(epoch)
+        new_valid_loss = validate(epoch)
+        if best_valid_loss is None or new_valid_loss < best_valid_loss:
+            best_valid_loss = new_valid_loss
+            print('Saving model with avg loss {}'.format(best_valid_loss))
+            torch.save(model.state_dict(),
+                       os.path.join(saved_model, 'params_epoch_{}_loss_{:.4f}.pt'.format(epoch, best_valid_loss)))
+
+        sample(epoch)
+
+    test()
