d8ff52cd4c184f4f713ede5ac7883cb6bbdec3c6
Mehrzad <mh.morta@gmail.com>
2019-04-18 02:32:03 -0400

    Updating Generator

--- gan_eval/models.py
+++ gan_eval/models.py
@@ -6,40 +6,63 @@ import torch
 ## Generator
 class Generator(nn.Module):
     """docstring for Generator"""
-    def __init__(self):
+    def __init__(self, img_size, latent_dim, dim):
         super(Generator, self).__init__()
-        self.model = nn.Sequential(
-            nn.Conv2d(3, 16, 3, padding=1),
-            nn.ReLU(),
-            nn.Dropout2d(p=0.25),
-            nn.Conv2d(16, 32, 3, padding=1),
-            nn.ReLU(),
-            nn.Dropout2d(p=0.25),
-            nn.MaxPool2d(2),
+        self.img_size = img_size
+        self.latend_dim = latent_dim
+        self.dim = dim
+        
+        # self.model = nn.Sequential(
+        #     nn.ConvTranspose2d(3, 16, 3, padding=1),
+        #     nn.ReLU(),
+        #     nn.Dropout2d(p=0.25),
+        #     nn.Conv2d(16, 32, 3, padding=1),
+        #     nn.ReLU(),
+        #     nn.Dropout2d(p=0.25),
+        #     nn.MaxPool2d(2),
 
-            nn.Conv2d(32, 32, 3, padding=1),
-            nn.ReLU(),
-            nn.Dropout2d(p=0.25),
-            nn.Conv2d(32, 64, 3, padding=1),
-            nn.ReLU(),
-            nn.Dropout2d(p=0.25),
-            nn.MaxPool2d(2),
+        #     nn.Conv2d(32, 32, 3, padding=1),
+        #     nn.ReLU(),
+        #     nn.Dropout2d(p=0.25),
+        #     nn.Conv2d(32, 64, 3, padding=1),
+        #     nn.ReLU(),
+        #     nn.Dropout2d(p=0.25),
+        #     nn.MaxPool2d(2),
 
-            nn.Conv2d(64, 64, 3, padding=1),
-            nn.ReLU(),
-            nn.Dropout2d(p=0.25),
-            nn.Conv2d(64, 128, 3, padding=1),
-            nn.ReLU(),
-            nn.Dropout2d(p=0.25),
-            nn.MaxPool2d(2),
+        #     nn.Conv2d(64, 64, 3, padding=1),
+        #     nn.ReLU(),
+        #     nn.Dropout2d(p=0.25),
+        #     nn.Conv2d(64, 128, 3, padding=1),
+        #     nn.ReLU(),
+        #     nn.Dropout2d(p=0.25),
+        #     nn.MaxPool2d(2),
 
-            nn.Conv2d(128, 512, 2),
-            )
+        #     nn.Conv2d(128, 512, 2),
+        #     )
         self.mlp = nn.Sequential(
             nn.ReLU(),
             nn.Dropout2d(p=0.5),
             nn.Linear(512,10),
             )
+    
+        self.model = nn.Sequential(
+            nn.ConvTranspose2d(8 * dim, 4 * dim, kernel_size=4, stride=2, padding=1),
+            nn.ReLU(),
+            nn.BatchNorm2d(4 * dim),
+            nn.ConvTranspose2d(4 * dim, 2 * dim, kernel_size=4, stride=2, padding=1),
+            nn.ReLU(),
+            nn.BatchNorm2d(2 * dim),
+            nn.ConvTranspose2d(2 * dim, dim, kernel_size=4, stride=2, padding=1),
+            nn.ReLU(),
+            nn.BatchNorm2d(dim),
+            nn.ConvTranspose2d(dim, self.img_size[2], kernel_size=4, stride=2, padding=1),
+            nn.Sigmoid()
+            )
+
+        self.mlp = nn.Sequential(
+            nn.Linear(latent_dim, 8 * dim * self.feature_sizes[0] * self.feature_sizes[1]),
+            nn.ReLU()
+                )
     def forward(self, x):
         return self.mlp(self.model(x)[:,:,0, 0])
 
