84e463dae53454dae56c2d98adf401407340c59f
Denis Ergashbaev <denis.ergashbaev@gmail.com>
2019-04-18 16:20:13 -0400

    working with comments

--- vae/eval.py
+++ vae/eval.py
@@ -11,29 +11,29 @@ from vae.train import model, device, current_dir
 # based on
 # http://bjlkeng.github.io/posts/importance-sampling-and-estimating-marginal-likelihood-in-variational-autoencoders/
 # https://github.com/bjlkeng/sandbox/blob/master/notebooks/vae-importance_sampling/vae-mnist-importance-sampling.ipynb
-def sample_z(model, X, num_samples):
-    mus, logvars = model.encode(X)
+def sample_z(model, x, num_samples):
+    mus, logvars = model.encode(x)
     z_samples = []
     qz = []
-
     for mu, logvar in zip(mus, logvars):
-        z_vals = torch.stack([model.reparameterize(mu, logvar) for _ in range(num_samples)])
+        z_vals = torch.stack([model.reparameterize(mu, logvar) for _ in range(num_samples)])  # (K x L)
+        qz_vals = [norm.pdf(zv.cpu(), loc=mu.cpu(), scale=np.exp(0.5 * logvar.cpu())) for zv in z_vals]  # (K x L)
         z_samples.append(z_vals)
-        qz.append([norm.pdf(zv.cpu(), loc=mu.cpu(), scale=np.exp(0.5 * logvar.cpu())) for zv in z_vals])
-    z_samples = torch.stack(z_samples)
-    qz = np.array(qz)
+        qz.append(qz_vals)
+    z_samples = torch.stack(z_samples)  # (M x K x L)
+    qz = np.array(qz)  # (M x K x L)
     return z_samples, qz
 
 
-def eval_log_px(model, X, z_samples, qz):
-    pz = norm.pdf(z_samples.cpu())
-    result = []
-    for x_input, z_sample, pz_i, qz_i in zip(X, z_samples, pz, qz):
-        x_recon = model.decode(z_sample)
+def eval_log_px(model, x, z_samples, qz):
+    pz = norm.pdf(z_samples.cpu())  # (M x K x L)
+    ret = []
+    for x_input, z_sample, pz_i, qz_i in zip(x, z_samples, pz, qz):
+        x_recon = model.decode(z_sample)  # x_recon: (K x 1 x 28 x 28), input: (1 x 28 x 28)
         log_pxz = []
         for x_r in x_recon:
             log_pxz.append(-F.binary_cross_entropy(x_r, x_input, reduction='sum'))
-        log_pxz = np.array(log_pxz)
+        log_pxz = np.array(log_pxz)  # (K)
         # See: scipy.special.logsumexp
         # \log p(x) = E_p[p(x|z)]
         # = \log(\int p(x|z) p(z) dz)
@@ -42,17 +42,18 @@ def eval_log_px(model, X, z_samples, qz):
         # ~= \log(1/n * \sum_i p(x|z_i) p(z_i)/q(z_i))
         # = \log p(x) = \log(1/n * \sum_i e^{\log p(x|z_i) + \log p(z_i) - \log q(z_i)})
         # = \log p(x) = -\logn + \logsumexp_i(\log p(x|z_i) + \log p(z_i) - \log q(z_i))
-        log_pz = np.sum(np.log(pz_i), axis=-1)
-        log_qz = np.sum(np.log(qz_i), axis=-1)
-        argsum = log_pxz + log_pz - log_qz
+        log_pz = np.sum(np.log(pz_i), axis=-1)  # (K)
+        log_qz = np.sum(np.log(qz_i), axis=-1)  # (K)
+        argsum = log_pxz + log_pz - log_qz  # (K)
         log_px = -np.log(len(argsum)) + logsumexp(argsum)
-        result.append(log_px)
+        ret.append(log_px)
 
-    return np.array(result)
+    ret = np.array(ret) # (M)
+    return ret
 
 
 with torch.no_grad():
-    # load examples from the valid_loader to save time because it takes too long to load
+    # load examples from the valid_loader to save time because it takes too long to load & split
     print('loading trained model')
     model.load_state_dict(torch.load('{}/best_model/params_epoch_20_loss_94.4314.pt'.format(current_dir), map_location=device))
     model.eval()
