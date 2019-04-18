df5267376932486b95b34ff9ae46a8d212bd6f55
Denis Ergashbaev <denis.ergashbaev@gmail.com>
2019-04-18 13:22:51 -0400

    before the split into a proper function

--- vae/eval.py
+++ vae/eval.py
@@ -1,40 +1,33 @@
-from vae.train import current_dir, model, device, loss_function
-from vae.dataloader import binarized_mnist_data_loader, MNIST_IMAGE_SIZE
 import numpy as np
-from scipy.stats import norm
-from scipy.special import logsumexp
 import torch
+from scipy.special import logsumexp
+from scipy.stats import norm
+
+from vae.train import model, device
 
 
 # based on
 # http://bjlkeng.github.io/posts/importance-sampling-and-estimating-marginal-likelihood-in-variational-autoencoders/
 # https://github.com/bjlkeng/sandbox/blob/master/notebooks/vae-importance_sampling/vae-mnist-importance-sampling.ipynb
-def compute_samples(data, num_samples, debug=False):
+def sample_z(data, num_samples, debug=False):
     """ Sample from importance distribution z_samples ~ q(z|X) and
         compute p(z_samples), q(z_samples) for importance sampling
     """
     mus, logvars = model.encode(data)
+    mus = mus.detach().numpy()
+    logvars = logvars.detach().numpy()
 
     z_samples = []
     qz = []
 
     for mu, logvar in zip(mus, logvars):
-        mu = mu.detach().numpy()
-        logvar = logvar.detach().numpy()
-        z_vals = [np.random.normal(mu[i], np.exp(logvar[i]), num_samples)
-                  for i in range(len(mu))]
-        qz_vals = [norm.pdf(z_vals[i], loc=mu[i], scale=np.exp(logvar[i]))
-                   for i in range(len(mu))]
+        z_vals = [model.reparameterize(torch.from_numpy(mu), torch.from_numpy(logvar)).detach().numpy() for _ in range(num_samples)]
         z_samples.append(z_vals)
-        qz.append(qz_vals)
+        qz.append([norm.pdf(zv, loc=mu, scale=np.exp(0.5 * logvar)) for zv in z_vals])
 
     z_samples = np.array(z_samples)
     pz = norm.pdf(z_samples)
     qz = np.array(qz)
-
-    z_samples = np.swapaxes(z_samples, 1, 2)
-    pz = np.swapaxes(pz, 1, 2)
-    qz = np.swapaxes(qz, 1, 2)
 
     if debug:
         print(mus.shape, logvars.shape)
@@ -51,7 +44,7 @@ def compute_samples(data, num_samples, debug=False):
 
 
 def eval_log_px(x, num_samples, debug=False):
-    z_samples, pz, qz = compute_samples(x, num_samples)
+    z_samples, pz, qz = sample_z(x, num_samples)
     z_samples = torch.from_numpy(z_samples).float()
     assert len(z_samples) == len(x)
     assert len(z_samples) == len(pz)
@@ -100,12 +93,12 @@ def eval_log_px(x, num_samples, debug=False):
 
 with torch.no_grad():
     # load examples from the valid_loader to save time because it takes too long to load
-    model.load_state_dict(torch.load('saved_model/params_epoch_9_loss_98.7259.pt', map_location=device))
+    model.load_state_dict(torch.load('best_model/params_epoch_20_loss_94.4314.pt', map_location=device))
     model.eval()
     for i in range(10):
         print('Batch ', i)
         X = torch.load('sample_input/batch_size_64/valid_{:03d}.pt'.format(i))
         ret = np.mean(eval_log_px(X, num_samples=128, debug=False))
         print('log(x): ', ret)
-        elbo = -loss_function(X, *model(X))
-        print('ELBO:', elbo)
+        elbo = -model.loss_function(X, *model(X))
+        print('ELBO:', elbo.item())
