c6688655dd165d9c9908ee32475557b8fe71ad63
Denis Ergashbaev <denis.ergashbaev@gmail.com>
2019-04-18 15:40:53 -0400

    let's try it out on the server

--- vae/eval.py
+++ vae/eval.py
@@ -2,45 +2,37 @@ import numpy as np
 import torch
 from scipy.special import logsumexp
 from scipy.stats import norm
-
-from vae.train import model, device
-from vae.dataloader import MNIST_IMAGE_SIZE
 from torch.nn import functional as F
 
+from vae.train import model, device
 
 
 # based on
 # http://bjlkeng.github.io/posts/importance-sampling-and-estimating-marginal-likelihood-in-variational-autoencoders/
 # https://github.com/bjlkeng/sandbox/blob/master/notebooks/vae-importance_sampling/vae-mnist-importance-sampling.ipynb
-def sample_z(data, num_samples):
-    mus, logvars = model.encode(data)
-    mus = mus.detach().numpy()
-    logvars = logvars.detach().numpy()
-
+def sample_z(model, X, num_samples):
+    mus, logvars = model.encode(X)
     z_samples = []
     qz = []
 
     for mu, logvar in zip(mus, logvars):
-        z_vals = [model.reparameterize(torch.from_numpy(mu), torch.from_numpy(logvar)).detach().numpy() for _ in range(num_samples)]
+        z_vals = torch.stack([model.reparameterize(mu, logvar) for _ in range(num_samples)])
         z_samples.append(z_vals)
         qz.append([norm.pdf(zv, loc=mu, scale=np.exp(0.5 * logvar)) for zv in z_vals])
-
-    z_samples = np.array(z_samples)
+    z_samples = torch.stack(z_samples)
     qz = np.array(qz)
     return z_samples, qz
 
 
-def eval_log_px(x, num_samples):
-    z_samples, qz = sample_z(x, num_samples)
+def eval_log_px(model, X, z_samples, qz):
     pz = norm.pdf(z_samples)
-    z_samples = torch.from_numpy(z_samples).float()
     result = []
-    for x_input, z_sample, pz_i, qz_i in zip(x, z_samples, pz, qz):
+    for x_input, z_sample, pz_i, qz_i in zip(X, z_samples, pz, qz):
         x_recon = model.decode(z_sample)
-        logp_xz = []
+        log_pxz = []
         for x_r in x_recon:
-            logp_xz.append(-F.binary_cross_entropy(x_r, x_input, reduction='sum'))
-        logp_xz = np.array(logp_xz)
+            log_pxz.append(-F.binary_cross_entropy(x_r, x_input, reduction='sum'))
+        log_pxz = np.array(log_pxz)
         # See: scipy.special.logsumexp
         # \log p(x) = E_p[p(x|z)]
         # = \log(\int p(x|z) p(z) dz)
@@ -51,7 +43,7 @@ def eval_log_px(x, num_samples):
         # = \log p(x) = -\logn + \logsumexp_i(\log p(x|z_i) + \log p(z_i) - \log q(z_i))
         log_pz = np.sum(np.log(pz_i), axis=-1)
         log_qz = np.sum(np.log(qz_i), axis=-1)
-        argsum = logp_xz + log_pz - log_qz
+        argsum = log_pxz + log_pz - log_qz
         log_px = -np.log(len(argsum)) + logsumexp(argsum)
         result.append(log_px)
 
@@ -64,8 +56,9 @@ with torch.no_grad():
     model.eval()
     for i in range(10):
         print('Batch ', i)
-        X = torch.load('sample_input/batch_size_64/valid_{:03d}.pt'.format(i))
-        ret = np.mean(eval_log_px(X, num_samples=200))
-        print('log(x): ', ret)
+        X = torch.load('split_mnist/batch_size_64/valid_{:03d}.pt'.format(i))
+        z_samples, qz = sample_z(model, X, num_samples=200)
+        ret = np.mean(eval_log_px(model, X, z_samples, qz))
+        print('log p(x): ', ret)
         elbo = -model.loss_function(X, *model(X))
         print('ELBO:', elbo.item())
--- vae/save_split_mnist.py
+++ vae/save_split_mnist.py
@@ -5,5 +5,5 @@ import torch
 _, valid_loader, _ = binarized_mnist_data_loader('{}/binarized_mnist'.format(current_dir), args.batch_size)
 
 for batch_idx, data in enumerate(valid_loader):
-    torch.save(data, '{}/sample_input/batch_size_{}/valid_{:03d}.pt'.
+    torch.save(data, '{}/split_mnist/batch_size_{}/valid_{:03d}.pt'.
                format(current_dir, args.batch_size, batch_idx))
